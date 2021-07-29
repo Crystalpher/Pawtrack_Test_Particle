4663644c85f4a4ff6fa3c8e7ecd4ef8d62ac44b8
Xingyu_Li <47982471+Crystalpher@users.noreply.github.com>
2021-07-29 16:51:37 +0800

    Update main_Fgeopack.m

--- main_Fgeopack.m
+++ main_Fgeopack.m
@@ -39,7 +39,8 @@ initialx = R_GSM(1)*1000.0;
 initialy = R_GSM(2)*1000.0;
 initialz = R_GSM(3)*1000.0;
 
-%% Find Mag-equator
+
+%% Find the Mag-equator's MLAT
 kext = onera_desp_lib_kext(11); % TS04
 
 inoptions = [0, 0, 0, 0, 0];
